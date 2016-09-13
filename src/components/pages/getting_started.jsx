d193c817d9d93684fcaed4b93cfe8f9c25f9e9dd
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-09-12 18:01:28 -0700

    Added more InputTypes and Getting Started documentation

--- src/components/pages/getting_started.jsx
+++ src/components/pages/getting_started.jsx
@@ -18,15 +18,16 @@ const GettingStarted = ({  }) => (
         <code>npm install --save react-formulate</code>
     </div>
     <div className="content">
+      <p>This project is actively under development. More features will be added in the near future (See <a href="https://www.github.com/gregchamberlain/react-formulate" target="_blank">github</a> README for a TODO list of future features.)! Suggestions and contributions are happily accepted!</p>
       <h1>Getting Started</h1>
       <p><c>react-formulate's</c> goal is to make it easier to create forms and controlled inputs in React. It includes a component to dynamically generate complete <c>Forms</c> from objects, as well as utilites to create controlled inputs for anything from a primative data type (string, number, boolean) to a complex, nested data structure of these data types (i.e. an object that holds values for multiple data types even including other objects and arrays).</p>
       <h2>Forms</h2>
       <p>A custom <c>Form</c> component is the default export of <c>react-formulate</c>, and will dynamically generate a form based on an object. Simply pass the object you wish to create a form for to the <c>from</c> prop of the <c>Form</c> component. The <c>Form</c> component also has an <c>onSubmit</c> prop, which takes a callback function to be invoked when the form has been submitted. This callback will receive a new object with the values from the form.</p>
       <Code value={BasicExample} readOnly/>
       <p>Default labels for each input are generated based on the object keys.</p>
-      <Example data={str} output={true} />
+      <Example data={str} output={true} readOnly={false}/>
       <h2>Controlled Inputs</h2>
-      <p>You may also wish to manually define the strucuture of your form. This gives you more control over what <Link to="input-types">InputType</Link> is used for each value. <c>react-formulate</c> has many additional exports that include these <Link to="input-types">InputTypes</Link>. All <Link to="input-types">InputTypes</Link> must be inoked as a function, optionally passing in an options object as an argument (see example below).</p>
+      <p><c>react-formulate</c> also includes utilities for building easier controlled inputs, that can handle complex data strucutres.  Additional exports from the package include multiple <Link to="input-types">InputTypes</Link> that allow for declarative construction of these controlled inputs. Each of these <Link to="input-types">InputTypes</Link> has a prop for value and onChange, which can be used just like the props on a normal React input. However the onChange prop will, by default, return the current value of the input instead of the event the input emmits. See the <Link to="input-types">InputTypes</Link> page for more information.</p>
       <Code value={DefinitionExample} readOnly/>
     </div>
   </div>
--- src/components/pages/input_types.jsx
+++ src/components/pages/input_types.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import * as InputTypes from 'react-formulate';
 
-const InputTypes = ({   }) => (
+const StringInput = InputTypes.string({label: 'String'});
+const NumberInput = InputTypes.number({label: 'Number'});
+const BooleanInput = InputTypes.boolean({label: 'Boolean'});
+const ArrayInput = InputTypes.array(InputTypes.string())({label: 'Strings'});
+const SelectInput = InputTypes.select({label: 'Select'})('One', 'Two', 'Three');
+const ColorInput = InputTypes.color({label: 'Color'});
+
+const InputTypesPage = ({   }) => (
   <div className="content">
     <h1>InputTypes</h1>
     <h2>string</h2>
+      <c>{"import { string } from 'react-formulate'"}</c><br />
+      <StringInput />
+      <h4>Usage</h4>
+      <c>
+        {"const StringInput = string({label: 'String Label'})"}<br />
+        {"<StringInput value={value} onChange={this.handleChange}/>"}
+      </c>
     <h2>number</h2>
+      <c>{"import { number } from 'react-formulate'"}</c><br />
+      <NumberInput />
     <h2>boolean</h2>
+      <c>{"import { boolean } from 'react-formulate'"}</c><br />
+      <BooleanInput />
     <h2>array</h2>
+      <c>{"import { array } from 'react-formulate'"}</c><br />
     <h2>object</h2>
+      <c>{"import { object } from 'react-formulate'"}</c>
+    <h2>select</h2>
+      <c>{"import { select } from 'react-formulate'"}</c><br />
+      <SelectInput />
   </div>
 );
 
-export default InputTypes;
+export default InputTypesPage;
