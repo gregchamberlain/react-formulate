f2d5cee64a55556e6cff657f0c626024358adf0d
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-09-25 18:09:18 -0700

    Updated select to take either objects or primatives

--- src/inputs/select.jsx
+++ src/inputs/select.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const select = (options, opts = {}) => {
-  const InputWrapper = ({value, onChange}) => (
-    <select value={value} onChange={e => onChange(e.target.value)}>
-      {options.map(option => <option value={option} key={option}>{option}</option>)}
-    </select>
+const select = (options = [], opts = {}) => {
+  const renderOptions = () => {
+    if (typeof options[0] === 'object') {
+      return options.map(option => (
+        <option value={option.value} key={option.name}>{option.name}</option>
+      ));
+    } else {
+      return options.map(option => (
+        <option value={option} key={option}>{option}</option>
+      ));
+    }
+  };
+  const InputWrapper = ({value, onChange, children}) => (
+    <label>
+      {opts.label}
+      <select value={value} onChange={e => onChange(e.target.value)}>
+        {renderOptions()}
+      </select>
+      {children}
+    </label>
   );
   InputWrapper.defaultValue = [];
   return InputWrapper;
